dd78e8cff5927a7b37734cacf58dd6634cd22482
harshit1142 <harshitpandey98765@gmail.com>
2026-01-28 22:53:39 +0530

    fix: deply sql connection and add db view endpoint

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false // This allows connection to Aiven's SSL-secured DB
+    }
 });
 
 const auth = new google.auth.GoogleAuth({
